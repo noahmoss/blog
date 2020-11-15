cf01c03c34b33fc861fac79ec97d27efc2c636ee
Noah Moss <noahbmoss@gmail.com>
2020-11-15 15:02:25 -0800

    tweak deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,24 +1,27 @@
 #!/bin/sh
-# Source: https://gohugo.io/hosting-and-deployment/hosting-on-github/
+# Original source: https://gohugo.io/hosting-and-deployment/hosting-on-github/
 
 # If a command fails then the deploy stops
 set -e
 
+cd ~/blog
+
+printf "\033[0;32mRebuilding site\033[0m\n"
 hugo -t hugo-theme-codex # if using a theme, replace with `hugo -t <YOURTHEME>`
 
-msg="rebuilding site $(date)"
+commit_msg="rebuilding site $(date)"
 if [ -n "$*" ]; then
-	msg="$*"
+	commit_msg="$*"
 fi
 
 cd public
 git add .
 printf "\033[0;32mDeploying build to GitHub...\033[0m\n"
-git commit -m "$msg"
+git commit -m "$commit_msg"
 git push origin master
 
 cd -
 git add .
 printf "\033[0;32mPushing source updates to GitHub...\033[0m\n"
-git commit -m "$msg"
+git commit -m "$commit_msg"
 git push origin master
