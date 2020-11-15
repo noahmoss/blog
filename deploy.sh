d76911ed5da1a29f7e0e1ed3cf90755e01dbb9da
Noah Moss <noahbmoss@gmail.com>
2020-11-15 10:08:13 -0800

    modify deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,30 +4,21 @@
 # If a command fails then the deploy stops
 set -e
 
-printf "\033[0;32mPushing source updates to GitHub...\033[0m\n"
+hugo -t hugo-theme-codex # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 msg="rebuilding site $(date)"
 if [ -n "$*" ]; then
 	msg="$*"
 fi
-git add .
-git commit -m "$msg"
-git push origin master
 
-# Build the project.
-hugo -t hugo-theme-codex # if using a theme, replace with `hugo -t <YOURTHEME>`
-
-# Go To Public folder
 cd public
-
-# Add changes to git.
 git add .
-
-# Commit changes.
 printf "\033[0;32mDeploying build to GitHub...\033[0m\n"
 git commit -m "$msg"
-
-# Push source and build repos.
 git push origin master
 
 cd -
+git add .
+printf "\033[0;32mPushing source updates to GitHub...\033[0m\n"
+git commit -m "$msg"
+git push origin master
